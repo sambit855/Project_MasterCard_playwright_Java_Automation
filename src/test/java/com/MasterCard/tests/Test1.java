f59e87f1f1653477364de1f011e72505c21bae76
Sambit <sambit855@gmail.com>
2026-03-14 00:47:25 +0530

    added new test file to the test folder

---
+++ src/test/java/com/MasterCard/tests/Test1.java
@@ -0,0 +1,27 @@
+package com.MasterCard.tests;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        Playwright playwright = Playwright.create();
+
+        Browser browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions().setHeadless(false)
+        );
+
+        Page page = browser.newPage();
+
+        page.navigate("https://www.google.com");
+
+        System.out.println("Page Title: " + page.title());
+
+        browser.close();
+        playwright.close();
+    }
+}
